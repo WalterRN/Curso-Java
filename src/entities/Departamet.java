d9179d688e0d0897d5c6cee698a59d24abdd9fde
Seu nomeWalter <walter.rutkowskineto@gmail.com>
2023-03-11 19:34:17 -0300

    New Worker

---
+++ src/entities/Departamet.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Departamet {
+	
+	private String name;
+
+	public Departamet() {
+	}
+
+	public Departamet(String name) {
+		
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
